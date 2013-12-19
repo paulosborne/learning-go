52c9db589733623dddb5389aff02eca3a5647073
vagrant <vagrant@precise64.(none)>
2013-12-19 14:01:14 +0000

    more examples

---
+++ go-by-example/structs.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type person struct {
+    name string
+    age int
+}
+
+func main() {
+    fmt.Println(person{"Bob", 20}) // {Bob 20}
+
+    fmt.Println(person{name: "Alice", age: 30}) // {Alice 30}
+
+    fmt.Println(person{name: "Fred"}) // {Fred 0}
+
+    fmt.Println(&person{name: "Ann", age: 40}) // &{Ann 40}
+
+    s := person{name: "Sean", age: 50}
+    fmt.Println(s.name) // Sean
+
+    sp := &s
+    fmt.Println(sp.age) // 50
+
+    sp.age = 51
+    fmt.Println(sp.age) // 51
+
+}
